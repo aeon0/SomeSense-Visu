5a9af87e33319b47b22862695fab9d812a2ab1d1
j-o-d-o <jdobler@protonmail.com>
2019-11-02 08:51:51 +0100

    properly reset world data

--- src/redux/world/reducer.ts
+++ src/redux/world/reducer.ts
@@ -5,8 +5,7 @@ import { IReduxWorld } from "./types"
 export default function(state: IReduxWorld = null, action: any) {
   switch (action.type) {
     case EReduxActionTypes.RESET_WORLD:
-      const resetState = {} as IReduxWorld;
-      return resetState;
+      return null;
     case EReduxActionTypes.UPDATE_WORLD:
       return action.world;
     default:
