9c590ab742380afdf99708b32ba0606ccd2d7179
j-o-d-o <jdobler@protonmail.com>
2020-12-24 15:53:57 +0100

    some warning here in babylonjs

--- src/world3D/algo_visu/cam/optical_flow_vis.ts
+++ src/world3D/algo_visu/cam/optical_flow_vis.ts
@@ -21,20 +21,20 @@ export class OpticalFlowVis extends IAlgoVis2D {
   public update(camSensor: CameraSensor, worldData: IReduxWorld) {
     this.reset();
 
-    for (let i = 0; i < worldData.camSensors.length; i++) {
-      if (camSensor.getKey() == worldData.camSensors[i].key) {
-        let flowLines: any = [];
-        worldData.camSensors[i].opticalFlow.flowTracks.forEach( track => {
-          const start = camSensor.calcImgToWorld(track.start);
-          const end = camSensor.calcImgToWorld(track.end);
+    // for (let i = 0; i < worldData.camSensors.length; i++) {
+    //   if (camSensor.getKey() == worldData.camSensors[i].key) {
+    //     let flowLines: any = [];
+    //     worldData.camSensors[i].opticalFlow.flowTracks.forEach( track => {
+    //       const start = camSensor.calcImgToWorld(track.start);
+    //       const end = camSensor.calcImgToWorld(track.end);
 
-          flowLines.push([start, end]);
-        });
+    //       flowLines.push([start, end]);
+    //     });
 
-        this.lineSys = MeshBuilder.CreateLineSystem("optical_flow_lines", {lines: flowLines, updatable: false}, this.scene);
-        this.lineSys.color = new Color3(0, 0, 1);
-        this.lineSys.renderingGroupId = 2;
-      }
-    }
+    //     this.lineSys = MeshBuilder.CreateLineSystem("optical_flow_lines", {lines: flowLines, updatable: false}, this.scene);
+    //     this.lineSys.color = new Color3(0, 0, 1);
+    //     this.lineSys.renderingGroupId = 2;
+    //   }
+    // }
   }
 }
