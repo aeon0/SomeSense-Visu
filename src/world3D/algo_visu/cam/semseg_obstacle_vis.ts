5bca76127af71b6f0dd2a31005ea831c92676190
j-o-d-o <jdobler@protonmail.com>
2021-03-26 11:54:26 +0100

    better performance with pcs

--- src/world3D/algo_visu/cam/semseg_obstacle_vis.ts
+++ src/world3D/algo_visu/cam/semseg_obstacle_vis.ts
@@ -1,55 +1,59 @@
-import { Scene, Vector3, Color4, MeshBuilder, SolidParticleSystem, Mesh, Scalar } from 'babylonjs'
+import { Scene, Vector3, Color4, PointsCloudSystem, CloudPoint } from 'babylonjs'
 import { IAlgoVis3D } from '../ivis'
 import { IReduxWorld } from '../../../redux/world/types'
+import { createGlobalStyle } from 'styled-components';
 
 
 export class SemsegObstacleVis extends IAlgoVis3D {
-  private SPS: SolidParticleSystem = null;
-  private model: Mesh = null;
+  private PCS: PointsCloudSystem = null;
 
   constructor(private scene: Scene) {
     super();
   }
 
   public reset() {
-    if (this.SPS) {
-      this.SPS.dispose();
-      this.model.dispose();
+    if (this.PCS) {
+      this.PCS.dispose();
+      this.PCS = null;
     }
   }
 
   public update(worldData: IReduxWorld) {
-    this.reset();
+    if (!this.PCS) {
+      this.PCS = new PointsCloudSystem("pcs", 1, this.scene);
+      this.PCS.addPoints(8000);
+      this.PCS.buildMeshAsync();
+    }
+    else {
+      var pointCloud: Vector3[] = worldData.obstacles;
 
-    this.SPS = new SolidParticleSystem('SPS_obstacle', this.scene);
-    this.model = MeshBuilder.CreateBox("m_obstacle", {size: 0.05}, this.scene);
+      const zeroVec = new Vector3(0, 0, 0);
+      this.PCS.updateParticle = (particle: CloudPoint) => {
+        if (particle.idx < pointCloud.length) {
+          const maxLength = 70;
+          const length = Math.min(pointCloud[particle.idx].length(), maxLength);
+          var r, g = 0;
+          if(length < (maxLength * 0.5)) {
+            r = 255;
+            g = Math.round(5.1 * length);
+          }
+          else {
+            g = 255;
+            r = Math.round(510 - 5.10 * length);
+          }
+          const color = new Color4(r/255.0, g/255.0, 0, 1.0);
 
-    var pointCloud: Vector3[] = worldData.obstacles;
-
-    this.SPS.initParticles = () => {
-      for (let p = 0; p < pointCloud.length - 1; p++) {
-        const maxLength = 70;
-        const length = Math.min(pointCloud[p].length(), maxLength);
-        var r, g = 0;
-        if(length < (maxLength * 0.5)) {
-          r = 255;
-          g = Math.round(5.1 * length);
+          particle.position = pointCloud[particle.idx];
+          particle.color = color;
         }
         else {
-          g = 255;
-          r = Math.round(510 - 5.10 * length);
+          particle.position = zeroVec;
         }
-        const color = new Color4(r/255.0, g/255.0, 0, 1.0);
+        return particle;
+      };
 
-        this.SPS.particles[p].position = pointCloud[p];
-        this.SPS.particles[p].color = color;
-      }
-    };
-
-    this.SPS.addShape(this.model, pointCloud.length);
-    this.SPS.buildMesh();
-    this.SPS.initParticles();
-    this.SPS.setParticles();
-    this.SPS.isAlwaysVisible = true;
+      this.PCS.setParticles();
+      this.PCS.isAlwaysVisible = true;
+    }
   }
 }
